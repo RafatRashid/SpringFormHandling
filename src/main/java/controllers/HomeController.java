84bb6887a077f419b37b6004cddfb68160678a0f
Rafat Rashid <rafatx003@gmail.com>
2017-11-15 20:33:51 +0600

    a form with validations using bean validation

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -3,9 +3,15 @@ package controllers;
 import models.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
 
 /**
  * Created by RAFATxRASHID on 13-Nov-17.
@@ -14,15 +20,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class HomeController
 {
     @GetMapping("/")
-    public String index(Model model)
+    public String form(Model model)
     {
         model.addAttribute("person", new Person());
         return "home";
     }
-
     @PostMapping("/")
-    public String process(@ModelAttribute Person person)
+    public String processForm(@Valid Person person, BindingResult bindingResult)
     {
+        person.setId(32);
+
+        if(bindingResult.hasErrors())
+            return "home";
         return "result";
     }
 }
--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -1,12 +1,44 @@
 package models;
 
+import org.hibernate.validator.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * Created by RAFATxRASHID on 14-Nov-17.
  */
 public class Person
 {
+    @NotNull
     int id;
+
+    @NotNull
+    @Size(min = 3, max = 25, message = "dude 3<-->25 man come onnn")
     String name;
+
+    @NotNull
+    @Email
+    String email;
+
+    @NotNull
+    @Size(min = 3, max=8)
+    String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public int getId() {
         return id;
@@ -22,5 +54,11 @@ public class Person
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: "+this.getId()+";Name: "+this
+                .getName()+"; Email: "+this.getEmail()+"; Password: "+this.getPassword()+";";
     }
 }
